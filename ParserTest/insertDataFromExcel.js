c2ebef3e579f827e680bd18fc9a0336334c5c5fe
IR <roi.ial@gmail.com>
2020-12-01 20:11:11 +0200

    Parsing the script_mobile.js file to an object

--- ParserTest/insertDataFromExcel.js
+++ ParserTest/insertDataFromExcel.js
@@ -1,13 +1,40 @@
 const fs = require("fs");
 
-let content = fs.readFileSync("script_mobile.js");
-// content.toString().split(/\n/).forEach(function(line){
-//     console.log(line);
-//   });
+let content = fs.readFileSync("./ParserTest/script_mobile.js");
+let trimmedContent = ""; 
+let shouldDeleteLine = false;
+content.toString().split(/\n/).forEach(function(line){
+    if (line.includes('trans('))
+    {
+        line = '\n';
+    }
+    else if (line.includes('"this."'))
+    {
+      line = line.replace('"this."', '"this.",');
+    }
+    else if (line.includes("scripts"))
+    {
+      line = '\n';
+      shouldDeleteLine = true;
+    }
+    else if (shouldDeleteLine)
+    {
+      if (line === '        },\r')
+      {        
+        shouldDeleteLine = false;
+      }
+      line = '\n';
+    }
+    trimmedContent += line;
+  });
 
-// console.log(content.toString());
 
-const substring = content.toString().substring(content.indexOf('var d = {') + 8, content.lastIndexOf('};') + 1);
-// console.log(content.toString().substring(content.indexOf('var d = {'), content.lastIndexOf('};') + 1));
+const substring = trimmedContent.toString().substring(trimmedContent.indexOf('var d = {') + 8, trimmedContent.lastIndexOf("if (d['data'] == undefined)") - 6);
+
+fs.writeFileSync('./ParserTest/output.json', substring, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
 let objectD = JSON.parse(substring);
 console.log('End');
