8dd689b8a7ab05d51e2b5b6c3d8a90b6e9d1e07c
IR <roi.ial@gmail.com>
2020-12-08 15:37:16 +0200

    Loop through cards and update + Break logic to funcitons + Apply beautify

--- insertDataFromExcel.js
+++ insertDataFromExcel.js
@@ -1,48 +1,86 @@
 const fs = require("fs");
+const beautify = require('js-beautify').js;
 
-let content = fs.readFileSync("script_mobile.js");
-let trimmedContent = ""; 
-let shouldDeleteLine = false;
-content.toString().split(/\n/).forEach(function(line){
-    if (line.includes('trans('))
-    {
-        line = line.replace('trans(', '"trans(');
-        line = line.replace(')', ')"');
-    }
-    else if (line.includes('"this."'))
-    {
-      line = line.replace('"this."', '"this.",');
-    }
-    else if (line.includes("scripts"))
-    {
-      line = '\n';
-      shouldDeleteLine = true;
-    }
-    else if (shouldDeleteLine)
-    {
-      if (line === '        },\r')
-      {        
-        shouldDeleteLine = false;
-      }
-      line = '\n';
-    }
-    trimmedContent += line;
-  });
+let objectD = parseFileToObject();
+let enFileContent = fs.readFileSync("en.txt");
+let enFileAsArray = enFileContent.toString().split(/\n/);
 
+// TODO: Create this array from excel file
+cardsData = [
+  {
+    cardName: "1009",
+    headerTitle: "Volvo Penta D2-75",
+    subTitle: "Volvo Penta D2-75 with saildrive and 4 blade Volvo Penta propeller"
+  },
+  {
+    cardName: "1024",
+    headerTitle: "DC ELECTRIC PANEL",
+    subTitle: "DC 12V ELECTRIC service PANEL"
+  },
+  {
+    cardName: "1025",
+    headerTitle: "AC ELECTRIC PANEL",
+    subTitle: "AC 115/230V ELECTRIC service PANEL"
+  }
+]
 
-const substring = trimmedContent.toString().substring(trimmedContent.indexOf('var d = {') + 8, trimmedContent.lastIndexOf("if (d['data'] == undefined)") - 6);
-
-fs.writeFileSync('output.json', substring, function (err) {
-  if (err) throw err;
-  console.log('Saved!');
+cardsData.forEach(card => {
+  let children = getCardChildren(card.cardName);
+  updateChildrenInEnFile(card, children);
 });
 
-let objectD = JSON.parse(substring);
+updateEnFile();
 
-const getCardChildren = () => {
+function parseFileToObject() {
+  let fileContent = fs.readFileSync("script_mobile.js");  
+  const substring = fileContent.toString().substring(fileContent.indexOf('var d = {') + 8, fileContent.lastIndexOf("if (d['data'] == undefined)") - 7);  
+  let beautifyContent = beautify(substring, { indent_size: 4, space_in_empty_paren: true });
+
+  let trimmedContent = ""; 
+  let shouldDeleteLine = false;
+  beautifyContent.toString().split(/\n/).forEach(function(line){
+      if (line.includes('trans('))
+      {
+          line = line.replace('trans(', '"trans(');
+          line = line.replace(')', ')"');
+      }
+      else if (line.includes('"this."'))
+      {
+        line = line.replace('"this."', '"this.",');
+      }
+      else if (line.includes("scripts"))
+      {
+        line = '\n';
+        shouldDeleteLine = true;
+      }
+      else if (shouldDeleteLine)
+      {
+        if (line === '    },')
+        {        
+          shouldDeleteLine = false;
+        }
+        line = '\n';
+      }
+      trimmedContent += line;
+    });
+
+  // For Debug:
+
+  // fs.writeFileSync('beautyObjectD.json', beautifyContent, function (err) {
+  //   if (err) throw err;
+  //  });
+
+  // fs.writeFileSync('parseableObjectD.json', trimmedContent, function (err) {
+  //   if (err) throw err;
+  //  });
+
+  return JSON.parse(trimmedContent);
+}
+
+function getCardChildren(cardName) {
   let children;
   objectD.definitions.forEach(card => {
-    if (card.data != undefined && card.data.name == '/1009')
+    if (card.data != undefined && card.data.name == '/' + cardName)
     {
       children = card.children;        
     }
@@ -50,42 +88,34 @@ const getCardChildren = () => {
   return children;
 }
 
-let cardChildren = getCardChildren();
-let enFileContent = fs.readFileSync("en.txt");
-let enFileAsArray = enFileContent.toString().split(/\n/);
-
-const replaceTextInEnFile = (itemText) => {
+function replaceTextInEnFile(childId, titleToReplace, itemText) {
   for (i = 0; i < enFileAsArray.length; i++)
   {
-    if (enFileAsArray[i].includes(itemText))
+    if (enFileAsArray[i].includes(childId))
     {
-      enFileAsArray[i] = enFileAsArray[i].replace('TextToReplace', 'Guy!');
+      enFileAsArray[i] = enFileAsArray[i].replace(titleToReplace, itemText);
     }
   }
 };
 
-cardChildren.forEach((child) => {
-  if (child.includes('HTMLText'))
-  {
-    let titleText = child.replace('this.', '');
-    replaceTextInEnFile(titleText);
-  }
-  else if (child.includes('Label_2FAB3382'))
-  {
-    let subTitleText = child.replace('this.', '');
-    replaceTextInEnFile(subTitleText);
-  }
-});
+function updateChildrenInEnFile(card, children) {
+  children.forEach((child) => {
+    let childId = child.replace('this.', '');
+    replaceTextInEnFile(childId, 'HeaderTitle', card.headerTitle);
+    replaceTextInEnFile(childId, 'SubTitle', card.subTitle)
+  });
+}
 
-let updatedEnFileContent;
-enFileAsArray.forEach(line => {
-  updatedEnFileContent += line;
-});
 
-fs.writeFileSync('newEn.txt', updatedEnFileContent, function (err) {
-  if (err) throw err;
-  console.log('Saved nwe en.txt file');
-});
+function updateEnFile() {
+  let updatedEnFileContent;
+  enFileAsArray.forEach(line => {
+    updatedEnFileContent += line;
+  });
 
-console.log(cardChildren);
+  fs.writeFileSync('newEn.txt', updatedEnFileContent, function (err) {
+    if (err) throw err;
+  });
+}
+
 console.log('End');
